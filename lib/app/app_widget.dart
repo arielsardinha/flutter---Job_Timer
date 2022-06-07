c629f842bd66eff707b5effc1035bf877bfbaa2e
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-06-07 16:57:33 -0300

    primeira aula

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart' as asuka;
+import 'package:job_time/app/core/ui/app_config_ui.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Job Time',
+      builder: asuka.builder,
+      theme: AppConfigUI.theme,
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
+  }
+}
